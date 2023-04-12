4ff8bb7d4d63ec5171f801834ef2b87c344a1eeb
nadhell <nadhell@live.com>
2023-04-12 11:13:51 +0200

    création branche nadege

--- microblogging-app/database/factories/PostFactory.php
+++ microblogging-app/database/factories/PostFactory.php
@@ -23,23 +23,3 @@ class PostFactory extends Factory
         ];
     }
 }
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
-class PostFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'description' => "Mon chat est trop mignon",
-            'img_url' => $this->faker->imageUrl(640, 480, 'cats'),
-            'user_id' => 1
-        ];
-    }
-}
--- microblogging-app/routes/web.php
+++ microblogging-app/routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
