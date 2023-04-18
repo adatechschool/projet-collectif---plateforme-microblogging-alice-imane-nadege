1396c0000982f1c97357719575666c79757f74db
Alice <darondeau.alice@gmail.com>
2023-04-18 12:31:53 +0200

    lien cliquable vers le profil des autres utilisateurs + adaptabilité de la route Mypage

--- microblogging-app/resources/views/posts/show.blade.php
+++ microblogging-app/resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
 <x-guest-layout>
     Posted on {{ $post->created_at }}
     <h2 class='font-semibold'>
-    {{$post->user->name}}
+    <a href="{{ route('mypage.another', ['id' => $post->user->id]) }}"> {{$post->user->name}}</a>
   </h2> 
     {{$post->description}}
   <img src="{{ $post->img_url }}">
